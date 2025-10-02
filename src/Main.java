9c3885097938822e4c38ff22a9224335de5c0120
Marg Jasper R. Cantere <canteremj12330@liceo.edu.ph>
2025-10-02 15:58:32 +0800

    Main.java
    added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        LibraryManager manager = new LibraryManager();
+
+
+        Book book1 = new Book("B01", "Java Programming", "James Gosling", "978-0135166307", 850, "Programming");
+        Book book2 = new Book("B02", "Clean Code", "Robert C. Martin", "978-0132350884", 464, "Software Engineering");
+
+        Magazine mag1 = new Magazine("M01", "Tech Today", "Editor Smith", 45, "September", true);
+        Magazine mag2 = new Magazine("M02", "Science Monthly", "Editor Jane", 12, "June", false);
+
+        DVD dvd1 = new DVD("D01", "The Matrix", "Wachowski Sisters", 136, "R", "Sci-Fi");
+        DVD dvd2 = new DVD("D02", "Finding Nemo", "Andrew Stanton", 100, "G", "Animation");
+
+
+        manager.addItem(book1);
+        manager.addItem(book2);
+        manager.addItem(mag1);
+        manager.addItem(mag2);
+        manager.addItem(dvd1);
+        manager.addItem(dvd2);
+
+
+        Student student = new Student("U01", "John Smith", "john@example.com", "S1001", "Computer Science");
+        Faculty faculty = new Faculty("U02", "Dr. Smith", "smith@example.edu", "Engineering", "Professor");
+
+
+        System.out.println("=== Displaying All Items ===");
+        manager.displayAllItems();
+        System.out.println();
+
+
+        System.out.println("=== Testing Borrowing ===");
+        if (manager.borrowItem("B01", student.getName())) {
+            student.addBorrowedItem(book1);
+            System.out.println("Student " + student.getName() + " borrowed: " + book1.getItemInfo());
+        }
+        if (manager.borrowItem("D01", faculty.getName())) {
+            faculty.addBorrowedItem(dvd1);
+            System.out.println("Faculty " + faculty.getName() + " borrowed: " + dvd1.getItemInfo());
+        }
+        System.out.println();
+
+
+        System.out.println("=== Displaying Available Items ===");
+        manager.displayAvailableItems();
+        System.out.println();
+
+
+        System.out.println("=== Testing Late Fees ===");
+        System.out.println(book1.getItemInfo() + " - 5 days late: $" + String.format("%.2f", book1.calculateLateFee(5)));
+        System.out.println(dvd1.getItemInfo() + " - 3 days late: $" + String.format("%.2f", dvd1.calculateLateFee(3)));
+        System.out.println();
+
+
+        System.out.println("=== Testing User Information ===");
+        System.out.println("Student: " + student.getName() + " (" + student.getMajor() + ") - " + student.getBorrowedItemsCount() + " items borrowed");
+        student.displayBorrowedItems();
+        System.out.println("Faculty: " + faculty.getName() + " (" + faculty.getDepartment() + ") - " + faculty.getBorrowedItemsCount() + " items borrowed");
+        faculty.displayBorrowedItems();
+        System.out.println();
+
+
+        System.out.println("=== Returning Items ===");
+        if (manager.returnItem("B01")) {
+            student.removeBorrowedItem(book1);
+            System.out.println("Returned: " + book1.getItemInfo());
+        }
+        System.out.println();
+
+
+        System.out.println("=== Final Available Items ===");
+        manager.displayAvailableItems();
+        System.out.println();
+
+
+        System.out.println("Total late fees for 3 days (all items): $" + String.format("%.2f", manager.calculateTotalLateFees(3)));
     }
 }
